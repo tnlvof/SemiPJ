5e2ac8fe9d072950792a3089853373c1118b77b1
user2 <user2@KH_C>
2018-03-13 20:16:26 +0900

    지도 완전바꿈,클래스,서블릿추가

--- semiProject/web/views/analysis/map/mapscript/geocode.js
+++ semiProject/web/views/analysis/map/mapscript/geocode.js
@@ -2,6 +2,8 @@
  * 
  */
 
+var marker = [];
+
 function trancode() {
 	var input_address = document.getElementById("address").value;
 	console.log(input_address);
@@ -25,4 +27,70 @@ function trancode() {
 		}
 	});
 
+}
+
+function addmarker(alley_arr) {
+	console.log(alley_arr);
+	var sel2 = document.getElementById("sel2").value;
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom : 16,
+		center : seoul
+	});
+
+	var geocoder = new google.maps.Geocoder();
+
+	for (var i = 0; i < alley_arr.length; i++) {
+		var address = alley_arr[i];
+
+		console.log(alley_arr[i]+"for");
+		geocoder.geocode({
+			'address' : address
+		}, function(results, status) {
+			console.log(status);
+			console.log(results);
+			if (status == google.maps.GeocoderStatus.OK) {
+				// var latLng = results[0].geometry.location;
+				var lat = results[0].geometry.location.lat();// 위도
+				var lng = results[0].geometry.location.lng();// 경도
+				var alley_nm=results[0].address_components[0].long_name;
+				console.log("alley_nm: "+alley_nm+" lat : " + lat + " lng : " + lng);
+
+				markerset(alley_nm, lat, lng);
+
+			}
+
+		});
+
+	}
+
+}
+function markerset(address, lat, lng) {
+	var sel2=document.getElementById("sel2").value;
+	addmarker = new google.maps.Marker({
+		position : {
+			lat : lat,
+			lng : lng
+		},
+		map : map,
+		title : address
+	});
+
+	console.log("마커 호출완료");
+
+	addmarker.addListener('click', function() {
+
+		console.log(address+sel2);
+		getCode(address,sel2)
+	});
+
+	addmarker.setMap(map);
+	map.panTo({
+		lat : lat,
+		lng : lng
+	});
+
+	marker.push(addmarker);
+
+	console.log(marker.length);
 }
