eb3458075a140237866cddc11cac20735550e3d2
sjh12 <sjh12@DESKTOP-5PO7OJ5>
2018-03-19 17:07:37 +0900

    2018/03/19 17:07 분석이력 삭제 버튼 서정호

--- semiProject/src/com/kh/pj/analyse/model/dao/AnalyseDao.java
+++ semiProject/src/com/kh/pj/analyse/model/dao/AnalyseDao.java
@@ -136,5 +136,21 @@ public class AnalyseDao {
 		}
 		return list;
 	}
+	public int deleteAnalyse(Connection con, String analysisNo) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		String query = prop.getProperty("deleteAnalyse");
+		try {
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, analysisNo);
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			close(pstmt);
+		}
+		return result;
+	}
 	
 }
